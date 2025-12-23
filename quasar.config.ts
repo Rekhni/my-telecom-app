0829b5a958e80005beec581bb722fda03a93260f
Rakhymzhan <mark.john.99@mail.ru>
2025-12-23 11:39:16 +0500

    Fix publicPath for Render

--- quasar.config.ts
+++ quasar.config.ts
@@ -36,7 +36,7 @@ export default defineConfig((/* ctx */) => {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
       },
-      publicPath: '/my-telecom-app/', 
+      publicPath: '/', 
       vueRouterMode: 'hash', 
 
       typescript: {
